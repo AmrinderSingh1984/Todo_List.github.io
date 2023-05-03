4eaf3bdeafd5eadfd64cb52f25bb62e5290f5895
AmrinderSingh1984 <108625549+AmrinderSingh1984@users.noreply.github.com>
2023-05-03 07:38:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MyForm from "./Components/MyForm"
+import { useState } from "react";
+
+function App() {
+  
+  return(
+    <>
+    <MyForm />
+    </>
+  )
+}
+
+
+export default App;
